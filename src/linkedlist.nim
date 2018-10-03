50d319278b7af04868536fb9fdbc7cd856dee9c9
Jamie Atkinson <jabbslad@gmail.com>
2018-10-04 00:24:59 +0100

    added append procs

--- src/linkedlist.nim
+++ src/linkedlist.nim
@@ -1,10 +1,15 @@
 type
   LinkedList*[T] = object
     head*: ref Node[T]
+    tail*: ref Node[T]
 
   Node*[T] = object
     data*: T
     next*: ref Node[T]
+
+proc newNode[T](data: T): ref Node[T] = 
+  new(result)
+  result.data = data
 
 proc newLinkedList*[T](): LinkedList[T] =
   discard
@@ -12,14 +17,24 @@ proc newLinkedList*[T](): LinkedList[T] =
 proc prepend*[T](L: var LinkedList[T], node: ref Node[T]) = 
   if L.head == nil:
     L.head = node
+    L.tail = node
   else:
     node.next = L.head
     L.head = node
 
 proc prepend*[T](L: var LinkedList[T], data: T) = 
-  var node = new(Node[T])
-  node.data = data
-  prepend(L, node)
+  prepend(L, newNode(data))
+
+proc append*[T](L: var LinkedList[T], node: ref Node[T]) =
+  if L.tail == nil:
+    L.tail = node
+    L.head = node
+  else:
+    node.next = L.tail
+    L.tail = node
+
+proc append*[T](L: var LinkedList[T], data: T) = 
+  append(L, newNode(data))
 
 iterator items*[T](L: LinkedList[T]): T =
   if L.head != nil:
--- tests/test1.nim
+++ tests/test1.nim
@@ -10,20 +10,36 @@ test "creates correct type":
 test "empty list":
   let ll = newLinkedList[int]()
   check ll.head == nil
+  check ll.tail == nil
 
-test "can prepend item":
+test "can prepend items":
   var ll = newLinkedList[string]()
   prepend(ll, "hello")
   check ll.head.data == "hello"
+  check ll.tail.data == "hello"
+  prepend(ll, "world")
+  check ll.head.data == "world"
+  check ll.tail.data == "hello"
+
+test "can append items":
+  var ll = newLinkedList[string]()
+  append(ll, "hello")
+  check ll.head.data == "hello"
+  check ll.tail.data == "hello"
+  append(ll, "world")
+  check ll.head.data == "hello"
+  check ll.tail.data == "world"
 
 test "items in correct order":
   var ll = newLinkedList[string]()
   prepend(ll, "hello")
   prepend(ll, "world")
-  check @["world", "hello"] == toSeq(items(ll))
+  prepend(ll, "goodbye")
+  check @["goodbye", "world", "hello"] == toSeq(items(ll))
 
 test "pairs in correct order":
   var ll = newLinkedList[string]()
   prepend(ll, "hello")
   prepend(ll, "world")
-  check @[(a: 0, b: "world"), (a: 1, b: "hello")] == toSeq(pairs(ll))
+  prepend(ll, "goodbye")
+  check @[(a: 0, b: "goodbye"), (a: 1, b: "world"), (a: 2, b: "hello")] == toSeq(pairs(ll))
