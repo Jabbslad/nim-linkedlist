5625f2063f86d63c334f7218b6892ed0a653f032
Jamie Atkinson <jabbslad@gmail.com>
2018-10-10 21:56:45 +0100

    implemented doublylinkedlist appending

--- src/linkedlist/doublylinkedlist.nim
+++ src/linkedlist/doublylinkedlist.nim
@@ -24,6 +24,19 @@ proc prepend*[T](L: var DoublyLinkedList[T], n: DoublyLinkedListNode[T]) =
 proc prepend*[T](L: var DoublyLinkedList[T], value: T) =
   prepend(L, newDoublyLinkedNode(value))
 
+proc append*[T](L: var DoublyLinkedList[T], n: DoublyLinkedListNode[T]) =
+  if L.tail == nil:
+    L.tail = n
+  else:
+    n.next = L.tail
+    L.tail = n
+
+  if L.head == nil:
+    L.head = L.tail
+
+proc append*[T](L: var DoublyLinkedList[T], value: T) =
+  append(L, newDoublyLinkedNode(value))
+
 iterator items*[T](L: var DoublyLinkedList[T]): T =
   var n = L.head
   while n != nil:
